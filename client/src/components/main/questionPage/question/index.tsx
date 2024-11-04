6af4b0e9397c6052e323b84d2894563aecb50bfe
neelavancha <avancha.n@northeastern.edu>
2024-11-04 12:28:32 -0500

    Committing change to navigate to Profile Page from Question view

--- client/src/components/main/questionPage/question/index.tsx
+++ client/src/components/main/questionPage/question/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './index.css';
 import { getMetaData } from '../../../../tool';
 import { Question } from '../../../../types';
+import UserProfileLink from '../../Profile/ProfileLink';
 
 /**
  * Interface representing the props for the Question component.
@@ -44,6 +45,10 @@ const QuestionView = ({ q }: QuestionProps) => {
     navigate(`/question/${questionID}`);
   };
 
+  const handleAuthorClick = (username: string) => {
+    navigate(`/profile/${username}`);
+  };
+
   return (
     <div
       className='question right_padding'
@@ -73,7 +78,14 @@ const QuestionView = ({ q }: QuestionProps) => {
         </div>
       </div>
       <div className='lastActivity'>
-        <div className='question_author'>{q.askedBy}</div>
+        <div
+          className='question_author'
+          onClick={e => {
+            e.stopPropagation();
+            handleAuthorClick(q.askedBy);
+          }}>
+          {q.askedBy}
+        </div>
         <div>&nbsp;</div>
         <div className='question_meta'>asked {getMetaData(new Date(q.askDateTime))}</div>
       </div>
