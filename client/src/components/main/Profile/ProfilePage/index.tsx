c78c88f20886be6d35532d8df7f9cfdc70d937d9
neelavancha <avancha.n@northeastern.edu>
2024-11-04 19:15:50 -0500

    Committing new dummy user

--- client/src/components/main/Profile/ProfilePage/index.tsx
+++ client/src/components/main/Profile/ProfilePage/index.tsx
@@ -1,10 +1,122 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import './index.css';
+import { Answer, Tag, Comment, Question} from '../../../../types';
 
 const ProfilePage = () => {
   const { username } = useParams();
 
+  const tag1: Tag = {
+    _id: '507f191e810c19729de860ea',
+    name: 'react',
+    description: 'T1_DESC',
+  };
+
+  const tag2: Tag = {
+    _id: '65e9a5c2b26199dbcc3e6dc8',
+    name: 'javascript',
+    description: 'T2_DESC',
+  };
+
+  const tag3: Tag = {
+    _id: '65e9b4b1766fca9451cba653',
+    name: 'android',
+    description: 'T3_DESC',
+  };
+
+  const com1: Comment = {
+    text: 'com1',
+    commentBy: 'com_by1',
+    commentDateTime: new Date('2023-11-18T09:25:00'),
+  };
+
+  const ans1: Answer = {
+    _id: '65e9b58910afe6e94fc6e6dc',
+    text: 'ans1',
+    ansBy: 'ansBy1',
+    ansDateTime: new Date('2023-11-18T09:24:00'),
+    comments: [],
+  };
+
+  const ans2: Answer = {
+    _id: '65e9b58910afe6e94fc6e6dd',
+    text: 'ans2',
+    ansBy: 'ansBy2',
+    ansDateTime: new Date('2023-11-20T09:24:00'),
+    comments: [],
+  };
+
+  const ans3: Answer = {
+    _id: '65e9b58910afe6e94fc6e6de',
+    text: 'ans3',
+    ansBy: 'ansBy3',
+    ansDateTime: new Date('2023-11-19T09:24:00'),
+    comments: [],
+  };
+
+  const ans4: Answer = {
+    _id: '65e9b58910afe6e94fc6e6df',
+    text: 'ans4',
+    ansBy: 'ansBy4',
+    ansDateTime: new Date('2023-11-19T09:24:00'),
+    comments: [],
+  };
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const QUESTIONS: Question[] = [
+    {
+      _id: '65e9b58910afe6e94fc6e6dc',
+      title: 'Quick question about storage on android',
+      text: 'I would like to know the best way to go about storing an array on an android phone so that even when the app/activity ended the data remains',
+      tags: [tag3, tag2],
+      answers: [ans1, ans2],
+      askedBy: 'q_by1',
+      askDateTime: new Date('2023-11-16T09:24:00'),
+      views: ['question1_user', 'question2_user'],
+      upVotes: [],
+      downVotes: [],
+      comments: [],
+    },
+    {
+      _id: '65e9b5a995b6c7045a30d823',
+      title: 'Object storage for a web application',
+      text: 'I am currently working on a website where, roughly 40 million documents and images should be served to its users. I need suggestions on which method is the most suitable for storing content with subject to these requirements.',
+      tags: [tag1, tag2],
+      answers: [ans1, ans2, ans3],
+      askedBy: 'q_by2',
+      askDateTime: new Date('2023-11-17T09:24:00'),
+      views: ['question2_user'],
+      upVotes: [],
+      downVotes: [],
+      comments: [],
+    },
+    {
+      _id: '65e9b9b44c052f0a08ecade0',
+      title: 'Is there a language to write programmes by pictures?',
+      text: 'Does something like that exist?',
+      tags: [],
+      answers: [],
+      askedBy: 'q_by3',
+      askDateTime: new Date('2023-11-19T09:24:00'),
+      views: ['question1_user', 'question2_user', 'question3_user', 'question4_user'],
+      upVotes: [],
+      downVotes: [],
+      comments: [],
+    },
+    {
+      _id: '65e9b716ff0e892116b2de09',
+      title: 'Unanswered Question #2',
+      text: 'Does something like that exist?',
+      tags: [],
+      answers: [],
+      askedBy: 'q_by4',
+      askDateTime: new Date('2023-11-20T09:24:00'),
+      views: [],
+      upVotes: [],
+      downVotes: [],
+      comments: [],
+    },
+  ];
   /**
    * In the later implementations of this page, we need a hook
    * to grab the Profile datatype associated with this username.
@@ -19,22 +131,13 @@ const ProfilePage = () => {
   const user = {
     name: username,
     title: 'Computer Engineer',
-    location: 'Turkey',
+    bio: 'Hello, I am a software developer simply trying to learn more on this platform! I specialize in Ubuntu, Information Security, and Software Engineering!',
     reputation: 2918,
     answers: 90,
     questions: 14,
-    peopleReached: '~39k',
-    communities: [
-      { name: 'Stack Overflow', rep: '2.9k' },
-      { name: 'Ask Ubuntu', rep: '806' },
-      { name: 'Information Security', rep: '186' },
-      { name: 'Software Engineering', rep: '142' },
-    ],
-    topTags: [
-      { name: 'python', score: 29, posts: 35, points: 34 },
-      { name: 'php', score: 10, posts: 16, points: 6 },
-      { name: 'mysql', score: 4, posts: 4 },
-    ],
+    joinedWhen: Date.now(),
+    questionsAsked: QUESTIONS,
+    answersGiven: [],
   };
 
   return (
