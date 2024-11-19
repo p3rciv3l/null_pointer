37e0392f1c5480c742fe649bef567a95641e15e0
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2024-11-18 21:51:55 -0500

    added useeditprofile and profile service

---
+++ client/src/services/profileService.ts
@@ -0,0 +1,38 @@
+import { Profile } from '../types';
+import api from './config';
+
+const PROFILE_API_URL = `${process.env.REACT_APP_SERVER_URL}/profile`;
+
+/**
+ * Function to update a profile.
+ *
+ * @param p - The profile object to update.
+ * @throws Error if there is an issue updating the profile.
+ */
+const updateProfile = async (p: Profile): Promise<Profile> => {
+  const res = await api.post(`${PROFILE_API_URL}/updateProfile`, p);
+
+  if (res.status !== 200) {
+    throw new Error('Error while updating a profile');
+  }
+
+  return res.data;
+};
+
+/**
+ * Function to get a profile by its username.
+ *
+ * @param username - The username of the profile to retrieve.
+ * @throws Error if there is an issue fetching the profile by username.
+ */
+const getProfile = async (username: string): Promise<Profile> => {
+  const res = await api.get(`${PROFILE_API_URL}/getProfile/${username}`);
+
+  if (res.status !== 200) {
+    throw new Error('Error when fetching profile by username');
+  }
+
+  return res.data;
+};
+
+export { getProfile, updateProfile };
