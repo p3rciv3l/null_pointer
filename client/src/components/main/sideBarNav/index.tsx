b48521501bd06749357f6da1186805c5349e5588
neelavancha <avancha.n@northeastern.edu>
2024-11-22 16:41:10 -0500

    Committing first change -- sidebar nav looking clean -- next up, header

--- client/src/components/main/sideBarNav/index.tsx
+++ client/src/components/main/sideBarNav/index.tsx
@@ -1,33 +1,54 @@
 import React from 'react';
 import './index.css';
 import { NavLink } from 'react-router-dom';
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { MessageSquare, Tag, User, Users } from 'lucide-react';
+import SideBarLink from './sidebarLink';
+import useUserContext from '../../../hooks/useUserContext';
 /**
  * The SideBarNav component has two menu items: "Questions" and "Tags".
  * It highlights the currently selected item based on the active page and
  * triggers corresponding functions when the menu items are clicked.
  */
-const SideBarNav = () => (
-  <div id='sideBarNav' className='sideBarNav'>
-    <NavLink
-      to='/home'
-      id='menu_questions'
-      className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
-      Questions
-    </NavLink>
-    <NavLink
-      to='/tags'
-      id='menu_tag'
-      className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
-      Tags
-    </NavLink>
-    <NavLink
-      to='/profile'
-      id='menu_tag'
-      className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
-      Profile
-    </NavLink>
-  </div>
-);
+
+const SideBarNav = () => {
+  const { user } = useUserContext(); // Access the user context
+
+  return (
+    <div id='sideBarNav' className='sideBarNav'>
+      <SideBarLink to='/home' icon={<MessageSquare />} label='Questions' id='menu_questions' />
+      <SideBarLink to='/tags' icon={<Tag />} label='Tags' id='menu_tags' />
+      <SideBarLink
+        to={`/profile/${user.username}`}
+        icon={<User />}
+        label='Profile'
+        id='menu_profile'
+      />
+    </div>
+  );
+};
+
+// const SideBarNav = () => (
+//   <div id='sideBarNav' className='sideBarNav'>
+//     <NavLink
+//       to='/home'
+//       id='menu_questions'
+//       className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
+//       Questions
+//     </NavLink>
+//     <NavLink
+//       to='/tags'
+//       id='menu_tag'
+//       className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
+//       Tags
+//     </NavLink>
+//     <NavLink
+//       to='/profile'
+//       id='menu_tag'
+//       className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
+//       Profile
+//     </NavLink>
+//   </div>
+// );
 
 export default SideBarNav;
