deb4c57a10c15e678e8656f7e39de3164616a9ec
neelavancha <avancha.n@northeastern.edu>
2024-11-22 11:53:53 -0500

    Committing first changes to displaying the Tags on the left-hand side of the screen

--- client/src/components/main/sideBarNav/index.tsx
+++ client/src/components/main/sideBarNav/index.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
 import './index.css';
 import { NavLink } from 'react-router-dom';
+import useTagPage from '../../../hooks/useTagPage';
 
 /**
  * The SideBarNav component has two menu items: "Questions" and "Tags".
  * It highlights the currently selected item based on the active page and
  * triggers corresponding functions when the menu items are clicked.
  */
-const SideBarNav = () => (
-  <div id='sideBarNav' className='sideBarNav'>
-    <NavLink
-      to='/home'
-      id='menu_questions'
-      className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
-      Questions
-    </NavLink>
-    <NavLink
-      to='/tags'
-      id='menu_tag'
-      className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
-      Tags
-    </NavLink>
-    <NavLink
-      to='/profile'
-      id='menu_tag'
-      className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
-      Profile
-    </NavLink>
-  </div>
-);
+const SideBarNav = () => {
+  const { tlist, clickTag } = useTagPage();
+
+  return (
+    <div id='sideBarNav' className='sideBarNav'>
+      <h2 className='sidebar-title'>Tags</h2>
+      <ul className='tag-list'>
+        {tlist.map((t, idx) => (
+          <li key={idx} className='tag-item' onClick={() => clickTag(t.name)}>
+            {t.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default SideBarNav;
