bead5016886719236c59dce9dc74b70873ec6100
neelavancha <avancha.n@northeastern.edu>
2024-11-24 10:39:09 -0500

    Starting the structural changes to the question answer page

--- client/src/components/main/answerPage/questionBody/index.tsx
+++ client/src/components/main/answerPage/questionBody/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './index.css';
 import { handleHyperlink } from '../../../../tool';
+import question from '../../questionPage/question';
+import VoteComponent from '../../voteComponent';
 
 /**
  * Interface representing the props for the QuestionBody component.
