5d345c891a2876211d73d947de060015b077fffe
neelavancha <avancha.n@northeastern.edu>
2024-11-29 22:31:27 -0500

    Cleaning up the AI Answer View and question, as well as adding more populate information in the script

--- client/src/components/main/answerPage/aiAnswerView/AIAnswerView.tsx
+++ client/src/components/main/answerPage/aiAnswerView/AIAnswerView.tsx
@@ -1,10 +1,7 @@
 // AIAnswerView.tsx
 import React, { useEffect, useState } from 'react';
-import { UserIcon } from 'lucide-react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Mistral } from '@mistralai/mistralai';
-import { getMetaData } from '../../../../tool';
-import { Comment } from '../../../../types';
 
 interface AIAnswerViewProps {
   questionText: string;
@@ -49,19 +46,10 @@ const AIAnswerView: React.FC<AIAnswerViewProps> = ({ questionText }) => {
     <div className='answer-container'>
       <div className='answer-content'>
         {isLoading ? (
-          <div className='loading'>Generating AI Answer...</div>
+          <div className='loading'>AI is thinking...</div>
         ) : (
           <>
             <div className='answer-text'>{aiAnswer}</div>
-            {/* <div className='author-card'>
-              <div className='author-details'>
-                <UserIcon className='author-icon' />
-                <div className='author-info'>
-                  <h3 className='author-name'>AI Assistant</h3>
-                  <p className='author-meta'>{getMetaData(new Date())}</p>
-                </div>
-              </div>
-            </div> */}
           </>
         )}
       </div>
