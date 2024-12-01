6fe36cb6c7e4a0f85ddebdcaa0f25a9c0c994210
p3rciv3l <avikrish001@gmail.com>
2024-12-01 02:10:31 -0500

    Visual fixes.

--- client/src/components/main/notificationBell/index.tsx
+++ client/src/components/main/notificationBell/index.tsx
@@ -5,10 +5,9 @@ import { Notification } from '../../../types';
 import './index.css';
 import NotificationComponent from './NotificationComponent';
 
-/* const BELL_ICON = '/bell_icon.png';
-const GREY_BELL_ICON = '/grey_bell.png'; */
 const BELL_ICON = '/assets/bell_icon.png';
 const GREY_BELL_ICON = '/assets/grey_bell.png';
+const HOVER_BELL_ICON = '/assets/hover_bell.png';
 
 const NotificationBell: React.FC = () => {
   const { socket, user } = useUserContext();
@@ -16,6 +15,7 @@ const NotificationBell: React.FC = () => {
   const [localNotifications, setLocalNotifications] = useState<Notification[]>(notifications);
   const [isOpen, setIsOpen] = useState(false);
   const [isGrey, setIsGrey] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -64,9 +64,18 @@ const NotificationBell: React.FC = () => {
       <NotificationComponent notifications={localNotifications} error={error} />
       <div
         className='bell-icon-container'
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}>
-        <img src={isGrey ? GREY_BELL_ICON : BELL_ICON} alt='notifications' />
+        onMouseEnter={() => {
+          setIsOpen(true);
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setIsOpen(false);
+          setIsHovered(false);
+        }}>
+        <img
+          src={isHovered ? HOVER_BELL_ICON : isGrey ? GREY_BELL_ICON : BELL_ICON}
+          alt='notifications'
+        />
         {localNotifications.length > 0 && (
           <span className='notification-count'>{localNotifications.length}</span>
         )}
